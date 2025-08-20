50b4fd8e0a5df43e83d2788846e2fa52d709b530
Gitmuseong <2530903@sw.hs.kr>
2025-08-20 14:18:06 +0900

    Update 정보과학.py

--- pages/정보과학.py
+++ pages/정보과학.py
@@ -1,10 +1,6 @@
 import streamlit as st
-st.set_page_config(
-    page_title="정보과학",
-    layout="centered"
-)
-
+st.set_page_config(page_title="정보과학", layout="centered")
 st.title("정보과학")
-st.subheader("깃허브")
-st.write("3학년 9반 이승민입니")
-
+st.subheader("Gitmuseong")
+st.write("3학년 9반 이승민입니다")
+st.link_button("연결할 url을 이 다음 변수에 써주세요!", 'https://docs.streamlit.io/develop/quick-reference/cheat-sheet')
