7df41a26c956f74643ccb0f2423860aa270570ef
Gitmuseong <2530903@sw.hs.kr>
2025-08-21 09:33:48 +0900

    Update 월드컵.py

--- pages/월드컵.py
+++ pages/월드컵.py
@@ -53,8 +53,8 @@ if not st.session_state.finished:
         else:
             random.shuffle(st.session_state.winners)
             st.session_state.pairs = [
-                st.session_state.winners[i], st.session_state.winners[i+1]
-                for i in range(0, len(st.session_state.winners), 2]
+                (st.session_state.winners[i], st.session_state.winners[i+1]
+                ,for i in range(0, len(st.session_state.winners), 2)]
             st.session_state.winners = []
             st.session_state.round += 1
             st.rerun()
